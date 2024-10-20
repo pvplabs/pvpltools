afefc1c725acb84b5d0d3fe64145dccec9ca41b8
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-10-20 18:20:39 +0100

    Improve maintenance instructions

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,13 @@ exclude_patterns = ["whatsnews/_next_release_template.rst"]
 # suppress unreferenced footnote warnings
 suppress_warnings = ["ref.footnote"]
 
+# rst Prolog is included at the beginning of every source file that is read.
+# This is useful for substitutions and other things that should be available in
+# every file, such as project-specific links.
+rst_prolog = """
+.. _PyPI_project: https://pypi.org/project/pvpltools/
+"""
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
